d5a28afd218cef11146d22ff0f9964883848fdf3
yassine meskaoui <yassine.meskaoui@gmail.com>
2016-10-27 12:40:22 +0200

    Creation javaBeans

---
+++ Mini_Projet_JEE/src/mini/jee/entities/Group.java
@@ -0,0 +1,34 @@
+package mini.jee.entities;
+
+public class Group {
+
+	private int idGroup;
+	private String NameGroup;
+
+	public Group() {
+		super();
+	}
+
+	public Group(int idGroup, String nameGroup) {
+		super();
+		this.idGroup = idGroup;
+		NameGroup = nameGroup;
+	}
+
+	public int getIdGroup() {
+		return idGroup;
+	}
+
+	public void setIdGroup(int idGroup) {
+		this.idGroup = idGroup;
+	}
+
+	public String getNameGroup() {
+		return NameGroup;
+	}
+
+	public void setNameGroup(String nameGroup) {
+		NameGroup = nameGroup;
+	}
+
+}
--- Mini_Projet_JEE/src/mini/jee/entities/Person.java
+++ Mini_Projet_JEE/src/mini/jee/entities/Person.java
@@ -1,7 +1,88 @@
 package mini.jee.entities;
 
+import java.util.Date;
+
 public class Person {
-	
-	
+
+	private int idPerson;
+	private String firstNamePerson;
+	private String lastNamePerson;
+	private String emailPerson;
+	private String webSitePerson;
+	private Date birthayPerson;
+	private int idGroupPerson;
+
+	public Person() {
+		super();
+	}
+
+	public Person(int idPerson, String firstNamePerson, String lastNamePerson, String emailPerson, String webSitePerson,
+			Date birthayPerson, int idGroupPerson) {
+		super();
+		this.idPerson = idPerson;
+		this.firstNamePerson = firstNamePerson;
+		this.lastNamePerson = lastNamePerson;
+		this.emailPerson = emailPerson;
+		this.webSitePerson = webSitePerson;
+		this.birthayPerson = birthayPerson;
+		this.idGroupPerson = idGroupPerson;
+	}
+
+	public int getIdPerson() {
+		return idPerson;
+	}
+
+	public void setIdPerson(int idPerson) {
+		this.idPerson = idPerson;
+	}
+
+	public String getFirstNamePerson() {
+		return firstNamePerson;
+	}
+
+	public void setFirstNamePerson(String firstNamePerson) {
+		this.firstNamePerson = firstNamePerson;
+	}
+
+	public String getLastNamePerson() {
+		return lastNamePerson;
+	}
+
+	public void setLastNamePerson(String lastNamePerson) {
+		this.lastNamePerson = lastNamePerson;
+	}
+
+	public String getEmailPerson() {
+		return emailPerson;
+	}
+
+	public void setEmailPerson(String emailPerson) {
+		this.emailPerson = emailPerson;
+	}
+
+	public String getWebSitePerson() {
+		return webSitePerson;
+	}
+
+	public void setWebSitePerson(String webSitePerson) {
+		this.webSitePerson = webSitePerson;
+	}
+
+	public Date getBirthayPerson() {
+		return birthayPerson;
+	}
+
+	public void setBirthayPerson(Date birthayPerson) {
+		this.birthayPerson = birthayPerson;
+	}
+
+	public int getIdGroupPerson() {
+		return idGroupPerson;
+	}
+
+	public void setIdGroupPerson(int idGroupPerson) {
+		this.idGroupPerson = idGroupPerson;
+	}
+
 
 }
